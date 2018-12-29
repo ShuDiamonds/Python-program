d11e2bf2ba90c1f4e87e511170f3c10e2f60bc49
ShuDiamonds <fukuda0729android@gmail.com>
2018-12-29 13:50:13 +0900

    review code

--- GoogleSpreadsheet/spreadsheetwrite.py
+++ GoogleSpreadsheet/spreadsheetwrite.py
@@ -3,6 +3,10 @@
 Created on Sat Dec 29 13:37:35 2018
 
 @author: shuichi
+
+spreadsheet setteing : https://qiita.com/akabei/items/0eac37cb852ad476c6b9
+
+spreadsheet function : https://tanuhack.com/python/library-gspread/
 """
 
 import gspread
@@ -11,9 +15,25 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('spreadsheet.json', scope)
-gc = gspread.authorize(credentials)
-wks = gc.open('gspread_sample').sheet1
 
-wks.update_acell('A1', 'Hello World!')
-print(wks.acell('A1'))
+if __name__ == "__main__":
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('spreadsheet.json', scope)
+    gc = gspread.authorize(credentials)
+    workbook = gc.open('gspread_sample')
+    worksheet = workbook.sheet1
+    print(workbook.title)	#スプレッドシートのタイトルを取得する
+    print(workbook.id)		#スプレッドシートキーを取得する
+    
+    # １．ラベルを指定してセルの値を取得する
+    cell_value = worksheet.acell('B1').value
+    
+    # ２．行番号と列番号を指定してセルの値を取得する（左：行番号、右：列番号）
+    cell_value = worksheet.cell(1, 2).value
+    
+    # ３．ラベルを指定して複数セルの値を一次元配列に格納する
+    range1 = worksheet.range('A1:B10')
+    
+    data=["time","value"]
+    worksheet.append_row(data)
+    
+    
